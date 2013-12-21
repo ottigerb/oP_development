72d8b086c775ee6e23a6d9aa5a80b58be8fd8c4d
Elise Huard <elise@jabberwocky.eu>
2013-12-21 13:29:49 +0000

    postgresql conf

--- manifests/db/postgres.pp
+++ manifests/db/postgres.pp
@@ -1,18 +1,13 @@
 class postgresql_bayhive {
   postgresql::db { 'op_prod':
-    user      => 'bhiveprod',
-    password  => 'lt4tad',
-  }
-
-  postgresql::db { 'therapy-server':
-    user      => 'elise1',
-    password  => 'mypass',
+    user      => 'therapy-server_production',
+    password  => 'xhqV3H3vbU',
   }
 
   postgresql::pg_hba_rule { 'puppet_connect_ident':
     description => "allow local md5 connection",
     type => 'local',
-    database => 'op_prod',
+    database => 'therapy-server_production',
     user => 'all',
     auth_method => 'ident',
     order=>'001'
@@ -21,7 +16,7 @@ class postgresql_bayhive {
 
 class { 'postgresql::server':
     config_hash => {
-        'postgres_password' => 'lt4tad',
+        'postgres_password' => 'xhqV3H3vbU',
         'ip_mask_deny_postgres_user' => '0.0.0.0/32',
         'ip_mask_allow_all_users' => '0.0.0.0/0',
         'listen_addresses' => '127.0.0.1'
